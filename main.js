211b113efe30d201b55f3ef428e0a26e443977a7
Edna Maria Farias Moreira <edna.maria887@gmail.com>
2022-09-29 23:22:25 -0300

    Ajuste dos eventos e estilos.

--- main.js
+++ main.js
@@ -1,22 +1,51 @@
+// variáveis 
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
 // fazendo a lógica com a criação do número aleatório
+var randomNumber = Math.round(Math.random() * 10)
+xAttemps = 1
 
-const randomNumber = Math.round(Math.random() * 10)
-let xAttemps = 1 // número de tentativas
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keypress', handleKeypress)
 
-// adicionando funções ao botão
-function handleClick(event) {
-  event.preventDefault() //para que apareça permanentemente quando eu clicar e não fique as configurações automáticas do meu form
 
-  const inputNumber = document.querySelector("#inputNumber")
 
-  //add caminhos 
+//funções callback
+function handleTryClick(event) {
+  event.preventDefault() // não faça o padrão do formulário ou não envie o formulário  - configurações automáticas do button dentro do meu form (que recarrega a página para enviar algum formulário)
+  
+  const inputNumber = document.querySelector('#inputNumber')
+
+  //add caminhos
   if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen1").classList.add("hide")
-    document.querySelector(".screen2").classList.remove("hide")
-
-    document.querySelector(".screen2 h2 span").innerText = xAttemps
+   toggleScreen()
+    screen2.querySelector('span').innerText = xAttemps
   }
+
+  inputNumber.value = "" // limpa o valor que estiver no input (caso eu erre)
   xAttemps++
 }
 
+function handleKeypress(e){
+  if ( e.key == "Enter" && screen1.classList.contains("hide")
+  //se na minha screen1 conter (contains) na lista de classes dele o hide
+  ) {
+     handleResetClick()
+  }
+ }
 
+function handleResetClick() {
+  toggleScreen()
+  xAttemps = 1
+  randomNumber = Math.round(Math.random() * 10)
+  // toda vez que eu resetar a minha aplicação vai ser gerado um novo número aleatório 
+}
+
+function toggleScreen() {
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
+}
